e48ce8447a7ef10cfdcf9557288cccbb54a031f3
Leandro <leandrowebmaster@gmail.com>
2025-05-19 13:59:34 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -422,7 +422,7 @@ borderRadius: "15px",
 'titleBox': true, 
 'hashtagBox': true, 
 'hashCheckBox': true, 'hashtagBoxChecked': false } 
-, 'inputs': { 'title': Exemplo de titulo } 
+, 'inputs': { 'title': dinossauros } 
  } 
  
 } 
