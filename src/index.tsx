3bedd5bbb81418e820fec4268e1d49b7f57dae3f
Leandro <leandrowebmaster@gmail.com>
2025-05-19 14:04:25 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -422,7 +422,7 @@ borderRadius: "15px",
 'titleBox': true, 
 'hashtagBox': true, 
 'hashCheckBox': true, 'hashtagBoxChecked': false } 
-, 'inputs': { 'title': ... } 
+, 'inputs': { 'title': Titulo Vazio } 
  } 
  
 } 
